17a92272d26d29546f4053313937e4691061ffa3
WXXW <oussama2wx@gmail.com>
2014-09-30 01:38:50 +0100

    ready to deploy

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   production: {
     rootPath: rootPath,
-    db: 'mongodb://oussamanasr:chillout@ds041160.mongolab.com:41160/chillout',
+    db: 'mongodb://oussama:chillout@ds041160.mongolab.com:41160/chillout',
     port: process.env.PORT || 80
   }
 }
